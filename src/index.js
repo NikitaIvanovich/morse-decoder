2bdd7ba68c9c3f5ff56cd8a36b70ba460782647c
Nikita <Nikita__99@mail.ru>
2022-08-08 10:49:29 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let resultArr = [];
+    for (let n = 0; n < expr.length; n += 10) {
+        let charArr = [];
+        for (let i = n; i < n + 10; i += 2) {
+            if (expr[i] === "1" && expr[i + 1] === "0") {
+                charArr.push(".")
+            }
+            if (expr[i] === "1" && expr[i + 1] === "1") {
+                charArr.push("-")
+            }
+            if (expr[i] === "*") {
+                resultArr.push(" ")
+                break;
+            }
+        }
+        for (let char in MORSE_TABLE) {
+            if (charArr.join('') === char) {
+                resultArr.push(MORSE_TABLE[char])
+            }
+        }
+    }
+    return resultArr.join('');
 }
 
 module.exports = {
